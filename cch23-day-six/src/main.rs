56342ae850c316cd8065bde568952f59b23f50a6
rootkill <rootkill.dedsec@gmail.com>
2023-12-10 02:40:58 +0530

    Solved task 2 for day 6

--- cch23-day-six/src/main.rs
+++ cch23-day-six/src/main.rs
@@ -8,6 +8,10 @@ use serde::Serialize;
 #[derive(Debug, Serialize)]
 struct Qelf {
     elf: usize,
+    #[serde(rename = "elf on a shelf")]
+    elf_on_a_shelf: usize,
+    #[serde(rename = "shelf with no elf on it")]
+    shelf_with_no_elf_on_it: usize,
 }
 
 async fn hello_world() -> &'static str {
@@ -15,8 +19,14 @@ async fn hello_world() -> &'static str {
 }
 
 async fn elfs(query: String) -> impl IntoResponse {
+    let elf = query.matches("elf").count();
+    let elf_on_a_shelf = query.matches("elf on a shelf").count();
+    let shelf_with_no_elf_on_it = query.matches("shelf").count() - elf_on_a_shelf;
+
     let res = Qelf {
-        elf: query.matches("elf").count(),
+        elf,
+        elf_on_a_shelf,
+        shelf_with_no_elf_on_it,
     };
 
     Json(res)
