c03b41641c9469209249f808821d6966641fd825
rootkill <rootkill.dedsec@gmail.com>
2023-12-09 12:54:54 +0530

    Solved Day 4 challenge

--- cch23-day-four/src/main.rs
+++ cch23-day-four/src/main.rs
@@ -1,12 +1,61 @@
-use axum::{routing::get, Router};
+mod utils;
+
+use axum::{
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::Deserialize;
+use utils::compare::compare;
+
+#[derive(Debug, Deserialize)]
+struct Reindeer {
+    // To mitigate the compiler warning about "name" not being used
+    // we added "_" prefix to name but the
+    // Data received will have variable "name" instead of "_name"
+    // so to properly deserialize the data received we use serde rename
+    #[serde(rename = "name")]
+    _name: String,
+    strength: i32,
+}
+
+#[derive(Debug, Deserialize)]
+struct ContestingReindeer {
+    name: String,
+    strength: i32,
+    speed: f64,
+    height: i32,
+    antler_width: i32,
+    snow_magic_power: i32,
+    favorite_food: String,
+    #[serde(rename = "cAnD13s_3ATeN-yesT3rdAy")]
+    candies_eaten_yesterday: i32,
+}
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn strength(Json(vec_reindeer): Json<Vec<Reindeer>>) -> impl IntoResponse {
+    println!("🦌 DATA = {:?}", vec_reindeer);
+
+    Json(vec_reindeer.iter().map(|r| r.strength).sum::<i32>())
+}
+
+async fn contest(Json(vec_contesting): Json<Vec<ContestingReindeer>>) -> impl IntoResponse {
+    println!("CONTESTING 🦌 LIST = {:?}", vec_contesting);
+
+    let result = compare(vec_contesting);
+
+    Json(result)
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/4/strength", post(strength))
+        .route("/4/contest", post(contest));
 
     Ok(router.into())
 }
---
+++ cch23-day-four/src/utils/compare.rs
@@ -0,0 +1,49 @@
+use serde::Serialize;
+
+use crate::ContestingReindeer;
+
+#[derive(Debug, Serialize)]
+pub struct ContestResult {
+    fastest: String,
+    tallest: String,
+    magician: String,
+    consumer: String,
+}
+
+pub fn compare(contest_reindeers: Vec<ContestingReindeer>) -> ContestResult {
+    let fastest = contest_reindeers
+        .iter()
+        .max_by(|r1, r2| r1.speed.total_cmp(&r2.speed))
+        .unwrap();
+
+    let tallest = contest_reindeers.iter().max_by_key(|r| r.height).unwrap();
+
+    let magician = contest_reindeers
+        .iter()
+        .max_by_key(|r| r.snow_magic_power)
+        .unwrap();
+
+    let consumer = contest_reindeers
+        .iter()
+        .max_by_key(|r| r.candies_eaten_yesterday)
+        .unwrap();
+
+    ContestResult {
+        fastest: format!(
+            "Speeding past the finish line with a strength of {} is {}",
+            fastest.strength, fastest.name
+        ),
+        tallest: format!(
+            "{} is standing tall with his {} cm wide antlers",
+            tallest.name, tallest.antler_width
+        ),
+        magician: format!(
+            "{} could blast you away with a snow magic power of {}",
+            magician.name, magician.snow_magic_power
+        ),
+        consumer: format!(
+            "{} ate lots of candies, but also some {}",
+            consumer.name, consumer.favorite_food
+        ),
+    }
+}
