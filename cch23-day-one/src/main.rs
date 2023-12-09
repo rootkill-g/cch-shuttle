9ac120a5745b479ab424116b799c57b6a597f034
rootkill <rootkill.dedsec@gmail.com>
2023-12-09 11:48:27 +0530

    Initial Commit

---
+++ cch23-day-one/src/main.rs
@@ -0,0 +1,52 @@
+use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Router};
+
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+async fn cube(Path(vec_path): Path<String>) -> impl IntoResponse {
+    println!("🧊 #> {:<12} - cube - {vec_path:?}", "HANDLER");
+
+    for c in vec_path.chars() {
+        if !c.is_ascii_digit() && c != '/' {
+            return (
+                StatusCode::BAD_REQUEST,
+                String::from("Invalid packets, only integers are allowed!"),
+            );
+        }
+    }
+
+    let separated: Vec<i32> = vec_path
+        .split('/')
+        .map(|v| v.parse::<i32>().unwrap())
+        .collect();
+
+    println!("Separated = {:?}", separated);
+
+    if separated.len() < 1 || separated.len() > 20 {
+        return (
+            StatusCode::BAD_REQUEST,
+            String::from("Invalid number of packets!"),
+        );
+    }
+
+    let mut xor_res = 0;
+
+    for pk in separated {
+        xor_res ^= pk;
+    }
+
+    let res = xor_res.pow(3);
+
+    (StatusCode::OK, format!("{}", res))
+}
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new()
+        .route("/", get(hello_world))
+        // .route("/1/:num1/:num2/:num3/:num4/:num5/:num6/:num7/:num8/:num9/:num10/:num11/:num12/:num13/:num14/:num15/:num16/:num17/:num18/:num19/:num20", get(cube));
+        .route("/1/*key", get(cube));
+
+    Ok(router.into())
+}
