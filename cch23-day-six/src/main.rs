5bee3f7eb6c54fc32b665f523b21c6017ff651be
rootkill <rootkill.dedsec@gmail.com>
2023-12-09 15:57:52 +0530

    Solved task 1 for day 6

--- cch23-day-six/src/main.rs
+++ cch23-day-six/src/main.rs
@@ -1,12 +1,32 @@
-use axum::{routing::get, Router};
+use axum::{
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+struct Qelf {
+    elf: usize,
+}
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn elfs(query: String) -> impl IntoResponse {
+    let res = Qelf {
+        elf: query.matches("elf").count(),
+    };
+
+    Json(res)
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/6", post(elfs));
 
     Ok(router.into())
 }
