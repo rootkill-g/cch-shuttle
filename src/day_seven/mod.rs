4d96d9b19fe0598f111e259facf2ece379af0a88
rootkill <rootkill.dedsec@gmail.com>
2023-12-11 01:21:27 +0530

    Updated function import from routes in day_seven module

--- src/day_seven/mod.rs
+++ src/day_seven/mod.rs
@@ -1,12 +1,10 @@
 mod routes;
 
 use axum::{routing::get, Router};
-use routes::decode;
-
-use self::routes::bake;
+use routes::{bake, decode};
 
 pub fn routes() -> Router {
     Router::new()
         .route("/7/decode", get(decode))
-        .route("/6/bake", get(bake))
+        .route("/7/bake", get(bake))
 }
