0e12189c14da09084c53021c7624fa01ee593db9
rootkill <rootkill.dedsec@gmail.com>
2023-12-24 17:27:54 +0530

    Updated day 5 with task 1

--- src/day_five/mod.rs
+++ src/day_five/mod.rs
@@ -1,8 +1,8 @@
 mod routes;
 
-use axum::{routing::get, Router};
+use axum::{routing::post, Router};
 use routes::msg_for_mr_grinch;
 
 pub fn routes() -> Router {
-    Router::new().route("/5", get(msg_for_mr_grinch))
+    Router::new().route("/5", post(msg_for_mr_grinch))
 }
--- src/day_five/routes.rs
+++ src/day_five/routes.rs
@@ -1,3 +1,27 @@
-pub async fn msg_for_mr_grinch() -> &'static str {
-    "We Want Day 5 Back MR. GRINCH!!!"
+use axum::{extract::Query, response::IntoResponse, Json};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct QueryParameters {
+    offset: Option<usize>,
+    limit: Option<usize>,
+}
+
+pub async fn msg_for_mr_grinch(
+    pagination: Query<QueryParameters>,
+    Json(names): Json<Vec<String>>,
+) -> impl IntoResponse {
+    tracing::info!("day_5 => POST RECEIVED: {:?}, {:?}", pagination, names);
+
+    let offset = pagination.offset.unwrap_or(0);
+
+    let list_slice = match pagination.limit {
+        Some(l) => {
+            let limit = names.len().min(offset + l);
+            &names[offset..limit]
+        }
+        None => &names[offset..],
+    };
+
+    Json(serde_json::json!(list_slice))
 }
