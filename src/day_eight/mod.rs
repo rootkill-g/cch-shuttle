d8d05371663ac053e008b969ff828b6b856fb3ef
rootkill <rootkill.dedsec@gmail.com>
2023-12-24 15:42:03 +0530

    Added momentum calculation handler for drop uri for task 2 of day 8

--- src/day_eight/mod.rs
+++ src/day_eight/mod.rs
@@ -3,6 +3,10 @@ mod routes;
 use axum::{routing::get, Router};
 use routes::poke_weight;
 
+use self::routes::poke_momentum;
+
 pub fn routes() -> Router {
-    Router::new().route("/8/weight/:pid", get(poke_weight))
+    Router::new()
+        .route("/8/weight/:pid", get(poke_weight))
+        .route("/8/drop/:pid", get(poke_momentum))
 }
--- src/day_eight/routes.rs
+++ src/day_eight/routes.rs
@@ -11,3 +11,17 @@ pub async fn poke_weight(Path(pid): Path<i64>) -> impl IntoResponse {
 
     Ok(format!("{}", (pokemon.weight as f64) / 10.0))
 }
+
+pub async fn poke_momentum(Path(pid): Path<i64>) -> impl IntoResponse {
+    tracing::info!("poke_momentum => GET: RECEIVED POKE_ID: {}", pid);
+
+    let pokemon_weight = match pokemon::get_by_id(pid, &RustemonClient::default()).await {
+        Ok(poke) => poke.weight as f64 / 10.0,
+        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR),
+    };
+
+    Ok(format!(
+        "{}",
+        pokemon_weight * (f64::sqrt(2.0 * 9.825 * 10.0))
+    ))
+}
