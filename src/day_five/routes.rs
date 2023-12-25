65b1fdc79cab8000a32ad7a9113a54d4639fa366
rootkill <rootkill.dedsec@gmail.com>
2023-12-25 15:30:57 +0530

    Updated day 5 for task 2

--- src/day_five/routes.rs
+++ src/day_five/routes.rs
@@ -1,10 +1,13 @@
 use axum::{extract::Query, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
 
+use serde_json as sj;
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct QueryParameters {
     offset: Option<usize>,
     limit: Option<usize>,
+    split: Option<usize>,
 }
 
 pub async fn msg_for_mr_grinch(
@@ -15,7 +18,7 @@ pub async fn msg_for_mr_grinch(
 
     let offset = pagination.offset.unwrap_or(0);
 
-    let list_slice = match pagination.limit {
+    let mut list_slice = match pagination.limit {
         Some(l) => {
             let limit = names.len().min(offset + l);
             &names[offset..limit]
@@ -23,5 +26,35 @@ pub async fn msg_for_mr_grinch(
         None => &names[offset..],
     };
 
-    Json(serde_json::json!(list_slice))
+    if list_slice.is_empty() {
+        return Json(sj::json!(list_slice));
+    }
+
+    let res = match pagination.split {
+        Some(s) => {
+            if s == 0 {
+                sj::json!(list_slice)
+            } else {
+                let mut result: Vec<Vec<String>> = Vec::new();
+
+                loop {
+                    if list_slice.len() <= s {
+                        result.push(list_slice.to_owned());
+                        break;
+                    }
+
+                    let (l, r) = list_slice.split_at(s);
+                    result.push(l.to_owned());
+                    if r.is_empty() {
+                        break;
+                    }
+                    list_slice = r;
+                }
+                sj::json!(result)
+            }
+        }
+        None => sj::json!(list_slice),
+    };
+
+    Json(res)
 }
